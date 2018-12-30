eb9daff6aae2a8e48db71be2e83ae8b9f6d4972a
wish911wish <wish911wish@gmail.com>
2018-12-30 11:17:31 +0900

    Add user_id column to Events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181229103355) do
+ActiveRecord::Schema.define(version: 20181230021250) do
 
   create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20181229103355) do
     t.datetime "start_time"
     t.datetime "end_time"
     t.text     "comment",    limit: 65535
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
