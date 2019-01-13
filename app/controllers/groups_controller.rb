3d0bf3e77b89f9a8be6f23a0deab37466235395c
wish911wish <wish911wish@gmail.com>
2019-01-13 11:36:45 +0900

    Creation of controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,42 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:show, :edit, :update]
+
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def edit
+  end
+
+  def create
+    @group = Group.new(group_params)
+    render :new unless @group.save
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private
+    def set_group
+      @group = Group.find(params[:id])
+    end
+
+
+    def group_params
+      params.require(:group).permit(:name)
+    end
+
 end
