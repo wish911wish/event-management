2f908291a445ed75034d1ba37b940fd6f93d331d
wish911wish <wish911wish@gmail.com>
2019-01-14 17:48:47 +0900

    Editing of users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_accessible :email, :password, :remember_me, :avatar, :avatar_cache, :remove_avatar
+
+  validates_presence_of   :avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
+
   has_many    :events
   has_many    :events, through: :attend_statuses
   has_many    :attend_statuses
