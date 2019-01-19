fd41658442536f2ca932559340a3f9c3293c66f9
wish911wish <wish911wish@gmail.com>
2019-01-19 18:27:08 +0900

    Implementation of  User page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def show
+    @created_events = Event.where('user_id = ?', current_user.id).order("updated_at DESC").page(params[:page]).per(10)
+    @attendance_events = current_user.events.order("updated_at DESC").page(params[:page]).per(10)
+  end
 end
