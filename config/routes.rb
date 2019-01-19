4018ac756bfca37ffa0211b5162e122692cb5407
wish911wish <wish911wish@gmail.com>
2019-01-19 19:00:04 +0900

    correction of attend routing that confriction to event routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'events#index'
   resources :events do
     resources :attend_statuses, only: [:create]
-    get '/:token' => 'attend_statuses#edit'
+    get 'attend_statuses/:token' => 'attend_statuses#edit'
   end
   resources :groups, except: [:destroy] do
     resources :group_members, only: [:create]
