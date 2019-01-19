1ca26419c80880892217fc96bd1bb98a85fdb2c0
wish911wish <wish911wish@gmail.com>
2019-01-19 18:29:23 +0900

    Implementation of image preview function

---
+++ app/assets/javascripts/user_avatar.js
@@ -0,0 +1,36 @@
+$(document).on("turbolinks:load", function() {
+
+  function previewUserImage(files){
+    resetPreview();
+    for (var i = 0; i < files.length; i++){
+      var file = files[i];
+      if (file.type.indexOf("image") < 0){
+        continue;
+      }
+      var img = document.createElement("img");
+      img.classList.add("previewImage");
+      img.file = file;
+      img.height = 200;
+      document.getElementsByClassName('image-display-area')[0].appendChild(img);
+      var reader = new FileReader();
+      reader.onload = (function(tImg) { return function(e) { tImg.src = e.target.result; }; })(img);
+      reader.readAsDataURL(file);
+    }
+  }
+
+  function resetPreview(){
+    var element = document.getElementsByClassName('image-display-area')[0]
+
+   while (element.firstChild) {
+      element.removeChild(element.firstChild);
+   }
+  }
+
+  $("#user_avatar").on("change", function(e) {
+    var file = e.target.files
+    previewUserImage(file)
+  });
+})
+
+
+
