0825dbe0874e6d28b295b255b1fe38a1668b26db
wish911wish <wish911wish@gmail.com>
2019-01-20 11:34:15 +0900

    Adding Image input form for group create function

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,13 @@
 class Group < ApplicationRecord
+  mount_uploader :image, AvatarUploader
   before_create :set_token
   has_many      :users, through: :group_members
   has_many      :group_members, dependent: :destroy
+  has_many      :events
+
+  validates_presence_of   :image
+  validates_integrity_of  :image
+  validates_processing_of :image
 
   private
   def set_token
