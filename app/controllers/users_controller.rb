743c1d9ca300876d3d4ed36aaab4605ab81b7fc8
wish911wish <wish911wish@gmail.com>
2019-01-20 13:53:06 +0900

    Display group information in each view file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @created_events = Event.where('user_id = ?', current_user.id).order("updated_at DESC").page(params[:page]).per(10)
     @attendance_events = current_user.events.order("updated_at DESC").page(params[:page]).per(10)
+    @groups = current_user.groups
   end
 end
