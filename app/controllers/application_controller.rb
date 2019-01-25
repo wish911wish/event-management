cae782d7f19dad22a886ca0717e1ee589947bc14
wish911wish <wish911wish@gmail.com>
2019-01-25 09:57:45 +0900

    Configuration of  redirect after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  # end
+  before_action :store_current_location, unless: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me, :image, :image_cache, :remove_image) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :image, :image_cache, :remove_image) }
     devise_parameter_sanitizer.permit(:edit) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :image, :image_cache, :remove_image) }
   end
+
+  private
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || user_path
+  end
+
 end
