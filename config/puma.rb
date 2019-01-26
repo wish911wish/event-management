3701393939b8edb01fe0c9067919fc7b2c59711b
wish911wish <wish911wish@gmail.com>
2019-01-26 19:55:01 +0900

    setting of SSL communication for development environment

--- config/puma.rb
+++ config/puma.rb
@@ -45,3 +45,10 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+if ENV.fetch('RAILS_ENV') { 'development' } == 'development'
+  ssl_bind '0.0.0.0', '9292', {
+    key: 'tmp/server.key',
+    cert: 'tmp/server.crt'
+  }
+end
